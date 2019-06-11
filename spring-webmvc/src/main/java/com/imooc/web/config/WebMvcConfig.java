497df246a94b2fa6428438eb4b3391dc6c6e99af
fengzikang <1697426994@qq.com>
2019-06-11 16:12:59 +0800

    几个常用注解

---
+++ spring-webmvc/src/main/java/com/imooc/web/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.imooc.web.config;
+
+
+//spring web mvc 配置
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig {
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
--- spring-webmvc/src/main/java/com/imooc/web/controller/HelloWorldController.java
+++ spring-webmvc/src/main/java/com/imooc/web/controller/HelloWorldController.java
@@ -1,13 +1,18 @@
 package com.imooc.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
 
     @RequestMapping("")
-    public String index(){
+    public String index(@RequestParam int p, Model model){
+        //model.addAttribute("acceptLanguage",acceptLanguage);
+        //model.addAttribute("jsessionId",jsessionId);
         return "index";
     }
+
+
 }
