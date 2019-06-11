5e3e192ebadc8751ad898242962d721f105249de
fengzikang <1697426994@qq.com>
2019-06-11 17:05:49 +0800

    spring 自动装配

---
+++ spring-webmvc/src/main/java/com/imooc/web/config/DispatcherServletConfiguration.java
@@ -0,0 +1,11 @@
+package com.imooc.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+
+
+/**
+ * {@link org.springframework.web.servlet.DispatcherServlet}
+ */
+@ComponentScan(basePackages = "com.imooc.web")
+public class DispatcherServletConfiguration {
+}
---
+++ spring-webmvc/src/main/java/com/imooc/web/servlet/support/DefaultAnnotationConfigDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package com.imooc.web.servlet.support;
+
+import com.imooc.web.config.DispatcherServletConfiguration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+// spring web MVC 自动装配 默认实现
+public class DefaultAnnotationConfigDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{DispatcherServletConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
